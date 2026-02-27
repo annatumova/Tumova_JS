24c39e69b1a508b7fb8445db9603891c0a668cf6
annatumova <tumovaanna4@gmail.com>
2026-02-27 10:40:40 +0100

    27. 2. 26

--- 3/cvičení/script.js
+++ 3/cvičení/script.js
@@ -1,10 +1,15 @@
 alert("Ahoj")
 
 let userName = "Pepíček";
-console.log(userName)
-
-
-userName = "Adélka";
-console.log(userName)
+console.log(userName);
 
 let age = 17; 
+console.log(age);
+
+userName = "Adélka";
+console.log(userName);
+
+let age = 16;
+console.log(age);
+
+console.log(17 + 16);
